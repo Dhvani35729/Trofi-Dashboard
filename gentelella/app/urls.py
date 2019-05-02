59a14b9b99a7b15529d652ff4b7ab941c21d4b42
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-05-02 16:09:59 -0600

    add favicon; connect login to firebase; connect routing; add logout

--- gentelella/app/urls.py
+++ gentelella/app/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     # Matches any html file - to be used for gentella
     # Avoid using your .html in your resources.
     # Or create a separate django app.
-    re_path(r'^.*\.html', views.gentella_html, name='gentella'),
+    # re_path(r'^.*\.html', views.gentella_html, name='gentella'),
 
     # The home page
     path('', views.index, name='index'),
+    path('login/', views.signIn, name='signIn'),
+    path('logout/', views.logout, name='logout'),
 ]
--- gentelella/app/views.py
+++ gentelella/app/views.py
@@ -1,13 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
+from django.contrib import auth
+import pyrebase
+import json
 
+config = {
+  "apiKey": "AIzaSyCwgogOI0rJDijj-r97dbWjEinKkrBH1Ok",
+  "authDomain": "daydesign-a277f.firebaseapp.com",
+  "databaseURL": "https://daydesign-a277f.firebaseio.com",
+  "storageBucket": "daydesign-a277f.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+# Get a reference to the auth service
+authe = firebase.auth()
+
+def logout(request):
+    auth.logout(request)
+    response = redirect('signIn')
+    return response
+
+def signIn(request):
+    try:
+        request.session['uid']
+        response = redirect('index')
+        return response   
+    except KeyError:
+        pass
+
+    email = request.POST.get("email")
+    passw = request.POST.get("password")
+
+    if not email or not passw:
+        context = {}
+        template = loader.get_template('app/login.html')
+        return HttpResponse(template.render(context, request))
+
+    # Log the user in
+    try:
+        user = authe.sign_in_with_email_and_password(email, passw)
+    except Exception as e:
+        error_json = e.args[1]        
+        error = json.loads(error_json)['error']        
+        message=error['message']
+        context = {"messg":message, "email": email, "passw": passw}
+        # response = redirect('signIn',context=context)
+        # return response
+        template = loader.get_template('app/login.html')        
+        return HttpResponse(template.render(context, request))
+
+    # print(user)
+    # print("debug")
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    response = redirect('index')
+    return response
 
 def index(request):
+    try:
+        request.session['uid']
+    except KeyError:
+        response = redirect('signIn')
+        return response   
+    print(request.session['uid'])
+
     context = {}
     template = loader.get_template('app/index.html')
     return HttpResponse(template.render(context, request))
-
 
 def gentella_html(request):
     context = {}
